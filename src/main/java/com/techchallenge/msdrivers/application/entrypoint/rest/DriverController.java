a651bbb687f66cb9e100ffc523c67a56bd61df6f
Emerson <lopes.ds@live.com>
2024-01-10 15:08:15 -0300

    feat() add find driver by id

--- src/main/java/com/techchallenge/msdrivers/application/entrypoint/rest/DriverController.java
+++ src/main/java/com/techchallenge/msdrivers/application/entrypoint/rest/DriverController.java
@@ -3,12 +3,15 @@ package com.techchallenge.msdrivers.application.entrypoint.rest;
 
 import com.techchallenge.msdrivers.application.entrypoint.rest.dto.DriverDTO;
 import com.techchallenge.msdrivers.application.mapper.driver.DriverMappers;
+import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteFindDriverByIdUseCase;
 import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteSaveDriverUseCase;
 import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteFindAllDriversUseCase;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/drivers")
@@ -18,16 +21,25 @@ public class DriverController {
 
     private final IExecuteFindAllDriversUseCase iExecuteFindAllDriversUseCase;
 
+    private final IExecuteFindDriverByIdUseCase iExecuteFindDriverByIdUseCase;
+
     public DriverController(
             IExecuteSaveDriverUseCase iExecuteSaveDriverUseCase,
-            IExecuteFindAllDriversUseCase iExecuteFindAllDriversUseCase) {
+            IExecuteFindAllDriversUseCase iExecuteFindAllDriversUseCase, IExecuteFindDriverByIdUseCase iExecuteFindDriverByIdUseCase) {
         this.iExecuteSaveDriverUseCase = iExecuteSaveDriverUseCase;
         this.iExecuteFindAllDriversUseCase = iExecuteFindAllDriversUseCase;
+        this.iExecuteFindDriverByIdUseCase = iExecuteFindDriverByIdUseCase;
     }
 
     @GetMapping
     public ResponseEntity<?> findDrivers() {
         final var response = iExecuteFindAllDriversUseCase.execute();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/{externalDriverId}")
+    public ResponseEntity<?> findDriverById(@PathVariable UUID externalDriverId) {
+        final var response = iExecuteFindDriverByIdUseCase.execute(externalDriverId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
