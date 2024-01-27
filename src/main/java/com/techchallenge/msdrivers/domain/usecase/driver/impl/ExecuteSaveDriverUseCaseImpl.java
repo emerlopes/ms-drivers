551c1afe644de60defd0dec0da6c044fe3c93282
Emerson <lopes.ds@live.com>
2024-01-27 11:31:59 -0300

    refact() removendo imports

--- src/main/java/com/techchallenge/msdrivers/domain/usecase/driver/impl/ExecuteSaveDriverUseCaseImpl.java
+++ src/main/java/com/techchallenge/msdrivers/domain/usecase/driver/impl/ExecuteSaveDriverUseCaseImpl.java
@@ -4,7 +4,6 @@ import com.techchallenge.msdrivers.application.shared.CustomData;
 import com.techchallenge.msdrivers.domain.entity.driver.DriverDomainEntityInput;
 import com.techchallenge.msdrivers.domain.entity.driver.DriverDomainEntityOutput;
 import com.techchallenge.msdrivers.domain.service.IDriverDomainService;
-import com.techchallenge.msdrivers.domain.service.IInscriptionDomainService;
 import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteSaveDriverUseCase;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +12,8 @@ public class ExecuteSaveDriverUseCaseImpl implements IExecuteSaveDriverUseCase {
 
     private final IDriverDomainService personService;
 
-    private final IInscriptionDomainService inscriptionService;
-
-    public ExecuteSaveDriverUseCaseImpl(IDriverDomainService personService, IInscriptionDomainService inscriptionService) {
+    public ExecuteSaveDriverUseCaseImpl(IDriverDomainService personService) {
         this.personService = personService;
-        this.inscriptionService = inscriptionService;
     }
 
     @Override
@@ -26,8 +22,6 @@ public class ExecuteSaveDriverUseCaseImpl implements IExecuteSaveDriverUseCase {
         final var driver = personService.createDriver(input);
         CustomData<DriverDomainEntityOutput> customData = new CustomData<>();
         customData.setData(driver);
-
-//        inscriptionService.createInscription(driver);
 
         return customData;
     }
