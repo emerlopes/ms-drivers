4f6717bfba31521a507b31cf2701f2181356d20d
Emerson <lopes.ds@live.com>
2024-01-29 01:01:36 -0300

    chore() criando documentacao

--- src/main/java/com/techchallenge/msdrivers/application/entrypoint/rest/DriverController.java
+++ src/main/java/com/techchallenge/msdrivers/application/entrypoint/rest/DriverController.java
@@ -2,9 +2,16 @@ package com.techchallenge.msdrivers.application.entrypoint.rest;
 
 import com.techchallenge.msdrivers.application.entrypoint.rest.dto.DriverDTO;
 import com.techchallenge.msdrivers.application.mapper.driver.DriverMappers;
+import com.techchallenge.msdrivers.application.shared.CustomData;
+import com.techchallenge.msdrivers.domain.entity.driver.DriverDomainEntityOutput;
+import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteFindAllDriversUseCase;
 import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteFindDriverByIdUseCase;
 import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteSaveDriverUseCase;
-import com.techchallenge.msdrivers.domain.usecase.driver.IExecuteFindAllDriversUseCase;
+import com.techchallenge.msdrivers.repositories.driversdatabase.entity.DriverEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -40,27 +47,26 @@ public class DriverController {
     }
 
     /**
-     * Endpoint para buscar todos os motoristas.
-     *
-     * @return ResponseEntity com a lista de motoristas e status HTTP OK (200).
-     */
-    @GetMapping
-    public ResponseEntity<?> findDrivers() {
-        final var response = iExecuteFindAllDriversUseCase.execute();
-        return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-    /**
      * Endpoint para buscar um motorista pelo ID externo.
      *
      * @param externalDriverId ID externo do motorista a ser buscado.
      * @return ResponseEntity com o motorista encontrado e status HTTP OK (200).
      */
-    @GetMapping("/{externalDriverId}")
-    public ResponseEntity<?> findDriverById(@PathVariable UUID externalDriverId) {
+    @Tag(name = "Driver", description = "API de Motoristas")
+    @Operation(
+            summary = "Buscar um motorista por ID externo",
+            description = "Recupera os dados de um motorista específico usando seu ID externo."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operação bem-sucedida, motorista encontrado e retornado.")
+    })
+    @GetMapping(value = "/{externalDriverId}", produces = "application/json")
+    public ResponseEntity<CustomData<DriverEntity>> findDriverById(@PathVariable UUID externalDriverId) {
+        // Substitua isso pela lógica real para buscar e formatar os dados do motorista conforme necessário
         final var response = iExecuteFindDriverByIdUseCase.execute(externalDriverId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
 
     /**
      * Endpoint para salvar um novo motorista.
@@ -68,12 +74,21 @@ public class DriverController {
      * @param driverDTO DTO contendo os dados do motorista a ser salvo.
      * @return ResponseEntity com o motorista salvo e status HTTP CREATED (201).
      */
+    @Tag(name = "Driver", description = "API de Motoristas")
+    @Operation(
+            summary = "Salvar um novo motorista",
+            description = "Salva uma nova entidade de motorista com os dados fornecidos no DriverDTO. Um CPF válido deve ser informado: para testes usar o 30030030030"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Operação bem-sucedida, novo motorista criado e retornado.")
+    })
     @PostMapping
-    public ResponseEntity<?> saveDriver(@Valid @RequestBody DriverDTO driverDTO) {
+    public ResponseEntity<CustomData<DriverDomainEntityOutput>> salvarMotorista(@Valid @RequestBody DriverDTO driverDTO) {
 
-        final var personDomainEntity = DriverMappers.mapToDriverDomainEntityInput(driverDTO);
-        final var response = iExecuteSaveDriverUseCase.execute(personDomainEntity);
+        final var entidadeDominioPessoa = DriverMappers.mapToDriverDomainEntityInput(driverDTO);
+        final var resposta = iExecuteSaveDriverUseCase.execute(entidadeDominioPessoa);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
     }
+
 }
