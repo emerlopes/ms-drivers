212f945a73d44e2ec89eb68c915f26f3922f119c
Emerson <lopes.ds@live.com>
2024-01-29 01:01:13 -0300

    chore() criando GlobalExceptionHandler

---
+++ src/main/java/com/techchallenge/msdrivers/application/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.techchallenge.msdrivers.application.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            if (error instanceof FieldError) {
+                String fieldName = ((FieldError) error).getField();
+                String errorMessage = error.getDefaultMessage();
+                errors.put(fieldName, errorMessage);
+            }
+        });
+
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "Validação falhou");
+        response.put("message", "Campos inválidos no DTO");
+        response.put("errors", errors.toString());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+}
