e7ad5d1104ed9d261e9b1d309e94ecc15f581e9e
Emerson <lopes.ds@live.com>
2024-01-29 22:53:56 -0300

    chore() add ExceptionHandler

--- src/main/java/com/techchallenge/msdrivers/application/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/techchallenge/msdrivers/application/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.techchallenge.msdrivers.application.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,5 +31,31 @@ public class GlobalExceptionHandler {
         response.put("errors", errors.toString());
 
         return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleUUIDFormatException(HttpMessageNotReadableException ex) {
+        Throwable cause = ex.getCause();
+        if (cause instanceof IllegalArgumentException && cause.getMessage().contains("UUID")) {
+            Map<String, String> response = new HashMap<>();
+            response.put("error", "Formato de UUID inválido");
+            response.put("message", "UUID deve ser representado pelo padrão de 36 caracteres");
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        return ResponseEntity.status(500).body(Map.of("error", "Erro na leitura da mensagem HTTP"));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        String message = ex.getMostSpecificCause().getMessage();
+
+        if (message != null && message.contains("NULL not allowed for column")) {
+            Map<String, String> response = new HashMap<>();
+            response.put("error", "Violação de Integridade de Dados");
+            response.put("message", "externalId nao encontrado " + message);
+            return ResponseEntity.badRequest().body(response);
+        }
+        return ResponseEntity.status(500).body(Map.of("error", "Erro de Integridade de Dados", "message", "Erro desconhecido de integridade de dados"));
     }
 }
